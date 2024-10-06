3d553894f3e196d7ade997ddd05d917e9d345046
Anik0047 <clananik@gmail.com>
2024-10-06 12:09:20 +0600

    conditional

---
+++ module-2/arithmetic.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+int main()
+{
+    // addition
+
+    // int a = 10, b = 50, sum;
+    // sum = a + b;
+    // printf("%d", sum);
+
+    // subtraction
+
+    // int a = 10, b = 50, sum;
+    // sum = b - a;
+    // printf("%d", sum);
+
+    // multiply
+
+    // int a = 10, b = 50, sum;
+    // sum = a * b;
+    // printf("%d", sum);
+
+    // division
+
+    // int a = 10, b = 50, sum;
+    // sum = b / a;
+    // printf("%d", sum);
+
+    // mod
+
+    // int a = 10, b = 56, sum;
+    // sum = b % a;
+    // printf("%d", sum);
+
+    // int x = 3, y = 20;
+    // float sum1 = y * 1.0 / x;
+    // printf("%f", sum1);
+
+    int x = 3, y = 20;
+    int sum1 = y * 1.0 / x;
+    printf("%f", sum1);
+
+    return 0;
+}
---
+++ module-2/conditional_1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    int taka;
+    scanf("%d", &taka);
+    if (taka >= 150)
+    {
+        printf("Ajke dekha korte parbo");
+    }
+    else
+    {
+        printf("taka nai re vai.");
+    };
+
+    return 0;
+}
